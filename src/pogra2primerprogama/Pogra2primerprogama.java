17d3c0262b4ff96545248b99f485f18b514da3c5
Wessly Zelaya <wess_zelaya@hotmail.com>
2022-07-22 23:36:30 -0600

    Primer Proyecto

---
+++ src/pogra2primerprogama/Pogra2primerprogama.java
@@ -0,0 +1,84 @@
+
+package pogra2primerprogama;
+import java.util.Scanner;
+
+public class Pogra2primerprogama {
+
+    public static void main(String[] args) {
+     Scanner entrada=new Scanner (System.in);
+
+   int opcion; 
+
+    System.out.println("Bienvenidos!");
+
+    System.out.println("Operadores Aritméticos");
+
+    System.out.println("1.Sumar enteros");
+
+    System.out.println("2.Restar enteros");
+
+    System.out.println("3.multiplicar enteros");
+
+    System.out.println("4.Dividir enteros");
+
+    System.out.println("Ingrese la opcion que desea elaborar: ");
+
+    opcion=entrada.nextInt();
+
+     
+
+    switch (opcion)
+
+    {
+
+      case 1 -> {
+          int val1, val2, suma;
+          System.out.println("Ingrese el primer valor: ");
+          val1=entrada.nextInt();
+          System.out.println("Ingrese el segundo valor: ");
+          val2=entrada.nextInt();
+          suma=val1+val2;
+          System.out.println("El total de la suma es: "+suma);
+            }
+
+      case 2 -> {
+          int val11,val22,resta;
+          System.out.println("Ingrese el primer valor: ");
+                int val1 = entrada.nextInt();
+                System.out.println("Ingrese el segundo valor: ");
+                int val2 = entrada.nextInt();
+                resta=val1-val2;
+                System.out.println("El total de la resta es: "+resta);
+            }
+
+      case 3 -> {
+          int val31,val32,multi;
+          System.out.println("Ingrese el primer valor: ");
+                int val1 = entrada.nextInt();
+                System.out.println("Ingrese el segundo valor: ");
+                int val2 = entrada.nextInt();
+                multi=val1*val2;
+                System.out.println("El total de la multiplicacion es: "+multi);
+            }
+
+      case 4 -> {
+          double val41,val42,div;
+          System.out.println("Ingrese el primer valor: ");
+                int val1 = entrada.nextInt();
+                System.out.println("Ingrese el segundo valor: ");
+                int val2 = entrada.nextInt();
+                div=val1/val2;
+                System.out.println("El total de la division es: "+div);
+            }
+
+      default -> System.out.print("Numero no valido");
+
+        
+
+        
+
+    }
+
+  }
+}
+    
